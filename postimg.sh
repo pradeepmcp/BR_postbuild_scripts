5c1c0b6a9ad6111d8e77fce1e09df562af540526
pradeepmcp <pradeepmcp@gmail.com>
2015-06-24 23:28:32 +0530

    Added network interface setup with DHCP.

--- postimg.sh
+++ postimg.sh
@@ -2,7 +2,7 @@
 
 TARGETDIR=$1
 BR_ROOT=$PWD
-CUST_SCRIPT=$BR_ROOT/postimgscripts
+CUST_SCRIPT=$BR_ROOT/postimgscripts/BR_postbuild_scripts
 CUST_FILES=$CUST_SCRIPT/customfiles
 OUTPUT=$BR_ROOT/output/target
 
@@ -19,3 +19,7 @@ cp $CUST_FILES/profile $OUTPUT/etc/
 # copy .bashrc
 #echo "Copying .bashrc"
 #cp $CUST_FILES/.bashrc $OUTPUT/root/
+
+# copy /etc/network/interfaces
+echo "Copying /etc/network/interfaces"
+cp $CUST_FILES/interfaces $OUTPUT/etc/network/
